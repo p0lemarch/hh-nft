e088168ad42d17ac2debae8a3e0498b8684f6942
p0lemarch <reginald@hatamoto.be>
2022-11-29 16:07:52 +0100

    random ipfs nft + tests

--- deploy/01-deploy-basic-nft.js
+++ deploy/01-deploy-basic-nft.js
@@ -2,7 +2,7 @@ const { network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
-module.exports = async function ({ getNamedAccounts, deployments }) {
+module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
 
---
+++ deploy/02-deploy-random-ipfs.js
@@ -0,0 +1,95 @@
+const { network, ethers } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
+const { developmentChains } = require("../helper-hardhat-config")
+const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata")
+
+const FUND_AMOUNT = "1000000000000000000000"
+const imagesLocation = "./images/randomNft"
+
+const metadataTemplate = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+        {
+            trait_type: "Cuteness",
+            value: 100,
+        },
+    ],
+}
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    console.log(chainId)
+
+    let tokenUris
+    // get the IPFS hashes of our images
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+
+    //1. With our own IPFS node: https://docs.ipfs.io
+    //2. Pinata
+
+    let vrfCoordinatorV2Address, subscriptionId
+    let vrfCoordinator
+
+    if (developmentChains.includes(network.name)) {
+        vrfCoordinator = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinator.address
+        const tx = await vrfCoordinator.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+        await vrfCoordinator.fundSubscription(subscriptionId, FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+    console.log("------------------------")
+    await storeImages(imagesLocation)
+
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane,
+        networkConfig[chainId].callbackGasLimit,
+        tokenUris,
+        networkConfig[chainId].mintFee,
+    ]
+    const randomIpfsNft = await deploy("RandomIpfsNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    if (chainId == 31337) {
+        await vrfCoordinator.addConsumer(subscriptionId, randomIpfsNft.address)
+    }
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(randomNft.address, args)
+    }
+    log("........")
+}
+
+async function handleTokenUris() {
+    tokenUris = []
+    const { responses: imageUploadResponses, files } = await storeImages(imagesLocation)
+    for (imageUploadResponseIndex in imageUploadResponses) {
+        let tokenUriMetadata = { ...metadataTemplate }
+        tokenUriMetadata.name = files[imageUploadResponseIndex].replace(".png", "")
+        tokenUriMetadata.description = `An adorable ${tokenUriMetadata.name} pup!`
+        tokenUriMetadata.image = `ipfs://${imageUploadResponses[imageUploadResponseIndex].IpfsHash}`
+        console.log(`Uploading ${tokenUriMetadata.name}...`)
+        const metadataUploadResponse = await storeTokenUriMetadata(tokenUriMetadata)
+        tokenUris.push(`ipfs://${metadataUploadResponse.IpfsHash}`)
+    }
+    console.log("Token URIs uploaded! They are:")
+    console.log(tokenUris)
+    return tokenUris
+}
+
+module.exports.tags = ["all", "randomipfs", "main"]
